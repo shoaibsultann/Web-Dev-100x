41183f8f55a3974018f467285d2f2c767e17b301
shoaibawan <shoaibawan0002@gmail.com>
2024-10-03 11:23:01 +0500

    JS function

---
+++ Week - 1/index.js
@@ -0,0 +1,17 @@
+var isStudent = true;
+console.log(isStudent);
+isStudent = "Shoaib"
+isStudent = 5;
+
+console.log(isStudent)
+
+
+function sum(a,b){
+  total = a +b;
+  return total;
+}
+
+let firstTotal = sum(10, 16)
+let secondTotal = sum("15", "40")
+console.log(firstTotal)
+console.log(secondTotal)
